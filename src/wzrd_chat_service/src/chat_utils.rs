22862d627305df9e7354b670fe2a6cc15f653243
crazycrab0x2 <maxbranch1110@gmail.com>
2023-12-05 09:02:20 -0500

    complete inter canister call

--- src/wzrd_chat_service/src/chat_utils.rs
+++ src/wzrd_chat_service/src/chat_utils.rs
@@ -1,3 +1,4 @@
+use candid::Principal;
 use ic_cdk::export::candid::CandidType;
 use std::cell::RefCell;
 use std::collections::BTreeMap;
@@ -52,17 +53,18 @@ pub async fn create_group(
     group_name: String, 
     group_description: Option<String>
 ) -> String {
-    let user_validation = ic_cdk::call::<(String,), (bool,)>(ic_cdk::caller(), "checkId", (id.clone(),)).await;
-    // if !res {
-    //     return "Invalid User ID".to_string();
-    // }
+
+    let user_validation = ic_cdk::call::<(String,), (bool,)>(Principal::from_text("be2us-64aaa-aaaaa-qaabq-cai").unwrap(), "checkId", (id.clone(),)).await;
     match user_validation {
-        Err(err) => {
-            return "Invalid User ID".to_string();
+        Err(_err) => {
+            return "Can't access store".to_string();
         }
         Ok(result) => {
-            let res = has_group_id(group_id.clone());
-            if res {
+            let (id_valid,): (bool,) = result;
+            if !id_valid {
+                return "Invalid User ID!".to_string();
+            }
+            if has_group_id(group_id.clone()) {
                 return "Group ID Alreay Exist".to_string();
             }
             GROUP_STORE.with(|group_store| {
@@ -89,71 +91,84 @@ pub async fn create_group(
     }    
 }
 
-pub fn join_group(
+pub async fn join_group(
     id: String, 
     group_id: String
 ) -> String {
-    // let mut res = id_utils::has_id(&id);
-    // if !res {
-    //     return "Invalid User ID".to_string();
-    // }
-    let res = has_group_id(group_id.clone());
-    if !res {
-        return "Invalid Group ID".to_string();
-    }
-    //""""
-    GROUP_STORE.with(|group_store| {
-        if let Some(group) = group_store.borrow_mut().iter_mut().find(|group| *group.group_id == group_id){
-            let mut new_members = group.group_members.clone();
-            if new_members.iter().find(|&member| *member == id).is_some(){
-                new_members.push(id.clone());
+    let user_validation = ic_cdk::call::<(String,), (bool,)>(Principal::from_text("be2us-64aaa-aaaaa-qaabq-cai").unwrap(), "checkId", (id.clone(),)).await;
+    match user_validation {
+        Err(_err) => {
+            return "Can't access store".to_string();
+        }
+        Ok(result) => {
+            let (id_valid,): (bool,) = result;
+            if !id_valid {
+                return "Invalid User ID!".to_string();
             }
-            group.group_members = new_members;
+            if !has_group_id(group_id.clone()) {
+                return "Invalid Group ID".to_string();
+            }
+            GROUP_STORE.with(|group_store| {
+                if let Some(group) = group_store.borrow_mut().iter_mut().find(|group| *group.group_id == group_id){
+                    let mut new_members = group.group_members.clone();
+                    if new_members.iter().find(|&member| *member == id).is_some(){
+                        new_members.push(id.clone());
+                    }
+                    group.group_members = new_members;
+                }
+            });
+            USER_GROUP_STORE.with(|user_group_store| {
+                let mut new_group_list;
+                if user_group_store.borrow().get(&id).is_none() {
+                    new_group_list = vec![];
+                } else {
+                    new_group_list = user_group_store.borrow().get(&id).unwrap().clone();
+                }
+                if !new_group_list.iter().find(|&group| *group == group_id).is_some(){
+                    new_group_list.push(group_id);
+                }
+                user_group_store.borrow_mut().insert(id, new_group_list);
+            });
+            "Success!".to_string()
         }
-    });
-    USER_GROUP_STORE.with(|user_group_store| {
-        let mut new_group_list;
-        if user_group_store.borrow().get(&id).is_none() {
-            new_group_list = vec![];
-        } else {
-            new_group_list = user_group_store.borrow().get(&id).unwrap().clone();
-        }
-        if !new_group_list.iter().find(|&group| *group == group_id).is_some(){
-            new_group_list.push(group_id);
-        }
-        user_group_store.borrow_mut().insert(id, new_group_list);
-    });
-    "Success!".to_string()
+    }
 }
 
-pub fn leave_group(
+pub async fn leave_group(
     id: String, 
     group_id: String
 ) -> String {
-    // let mut res = id_utils::has_id(&id);
-    // if !res {
-    //     return "Invalid User ID".to_string();
-    // }
-    let res = has_group_id(group_id.clone());
-    if !res {
-        return "Invalid Group ID".to_string();
+    let user_validation = ic_cdk::call::<(String,), (bool,)>(Principal::from_text("be2us-64aaa-aaaaa-qaabq-cai").unwrap(), "checkId", (id.clone(),)).await;
+    match user_validation {
+        Err(_err) => {
+            return "Can't access store".to_string();
+        }
+        Ok(result) => {
+            let (id_valid,): (bool,) = result;
+            if !id_valid {
+                return "Invalid User ID!".to_string();
+            }
+            if !has_group_id(group_id.clone()) {
+                return "Invalid Group ID".to_string();
+            }
+            GROUP_STORE.with(|group_store| {
+                if let Some(group) = group_store.borrow_mut().iter_mut().find(|group| *group.group_id == group_id){
+                    let mut new_members = group.group_members.clone();
+                    new_members.retain(|member| *member != id);
+                    group.group_members = new_members;
+                }
+            });
+            USER_GROUP_STORE.with(|user_group_store| {
+                let mut new_group_list;
+                if !user_group_store.borrow().get(&id).is_none() {
+                    new_group_list = user_group_store.borrow().get(&id).unwrap().clone();
+                    new_group_list.retain(|groupid| *groupid != group_id);
+                    user_group_store.borrow_mut().insert(id, new_group_list);
+                }
+            });
+            "Success!".to_string()
+        }
     }
-    GROUP_STORE.with(|group_store| {
-        if let Some(group) = group_store.borrow_mut().iter_mut().find(|group| *group.group_id == group_id){
-            let mut new_members = group.group_members.clone();
-            new_members.retain(|member| *member != id);
-            group.group_members = new_members;
-        }
-    });
-    USER_GROUP_STORE.with(|user_group_store| {
-        let mut new_group_list;
-        if !user_group_store.borrow().get(&id).is_none() {
-            new_group_list = user_group_store.borrow().get(&id).unwrap().clone();
-            new_group_list.retain(|groupid| *groupid != group_id);
-            user_group_store.borrow_mut().insert(id, new_group_list);
-        }
-    });
-    "Success!".to_string()
 }                                      
 
 pub fn get_group_members(
@@ -191,95 +206,118 @@ pub fn get_group_messages(
     }
 }
 
-pub fn send_group_message(
+pub async fn send_group_message(
     id: String, 
     group_id: String, 
     reply_id: Option<String>,
     content: String
 ) -> String {
-    // let mut res = id_utils::has_id(&id);
-    // if !res {
-    //     return "Invalid User ID".to_string();
-    // }
-    let res = has_group_id(group_id.clone());
-    if !res {
-        return "Invalid Group ID".to_string();
+    let user_validation = ic_cdk::call::<(String,), (bool,)>(Principal::from_text("be2us-64aaa-aaaaa-qaabq-cai").unwrap(), "checkId", (id.clone(),)).await;
+    match user_validation {
+        Err(_err) => {
+            return "Can't access store".to_string();
+        }
+        Ok(result) => {
+            let (id_valid,): (bool,) = result;
+            if !id_valid {
+                return "Invalid User ID!".to_string();
+            } 
+            if !has_group_id(group_id.clone()) {
+                return "Invalid Group ID".to_string();
+            }
+            GROUP_MESSAGE_STORE.with(|group_message_store| {
+                let mut new_message_list;
+                let message_id;
+                if group_message_store.borrow().get(&group_id).is_none() {
+                    new_message_list = vec![];
+                    message_id = 0;
+                }
+                else{
+                    new_message_list = group_message_store.borrow().get(&group_id).unwrap().clone();
+                    message_id = new_message_list.len();
+                }
+                let message = GroupMessage { 
+                    id: message_id, 
+                    sender_id: id, 
+                    reply_id,
+                    content, 
+                    timestamp: "SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis()".to_string()
+                };
+                new_message_list.push(message);
+                group_message_store.borrow_mut().insert(group_id, new_message_list);
+            });
+            "Success!".to_string()
+        }
     }
-    GROUP_MESSAGE_STORE.with(|group_message_store| {
-        let mut new_message_list;
-        let message_id;
-        if group_message_store.borrow().get(&group_id).is_none() {
-            new_message_list = vec![];
-            message_id = 0;
-        }
-        else{
-            new_message_list = group_message_store.borrow().get(&group_id).unwrap().clone();
-            message_id = new_message_list.len();
-        }
-        let message = GroupMessage { 
-            id: message_id, 
-            sender_id: id, 
-            reply_id,
-            content, 
-            timestamp: "SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis()".to_string()
-        };
-        new_message_list.push(message);
-        group_message_store.borrow_mut().insert(group_id, new_message_list);
-    });
-    "Success!".to_string()
 }
 
-pub fn send_direct_message(
+pub async fn send_direct_message(
     sender_id: String, 
     receiver_id: String, 
     reply_id: Option<String>,
     content: String
 ) -> String {
-    // let mut res = id_utils::has_id(&sender_id);
-    // if !res {
-    //     return "Invalid Sender ID".to_string();
-    // }
-    // res = id_utils::has_id(&receiver_id);
-    // if !res {
-    //     return "Invalid Receiver ID".to_string();
-    // }
-    DIRECT_MESSAGE_STORE.with(|direct_message_store| {
-        let message = DirectMessage { 
-            id: direct_message_store.borrow().clone().len(), 
-            sender_id: sender_id.clone(), 
-            receiver_id: receiver_id.clone(), 
-            reply_id,
-            content, 
-            timestamp: "SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis()".to_string(), 
-            // timestamp: DateTime::now().to_string(),            
-            viewed: false 
-        };
-        direct_message_store.borrow_mut().push(message);
-    });
-    USER_FRIEND_STORE.with(|user_friend_store| {
-        let mut sender_friend_list;
-        let mut receiver_friend_list;
-        if user_friend_store.borrow().get(&sender_id).is_none() {
-            sender_friend_list = vec![];
-        } else {
-            sender_friend_list = user_friend_store.borrow().get(&sender_id).unwrap().clone();
+    let sender_validation = ic_cdk::call::<(String,), (bool,)>(Principal::from_text("be2us-64aaa-aaaaa-qaabq-cai").unwrap(), "checkId", (sender_id.clone(),)).await;
+    match sender_validation {
+        Err(_err) => {
+            return "Can't access store".to_string();
         }
-        if !sender_friend_list.iter().find(|&id| *id == receiver_id).is_some(){
-            sender_friend_list.push(receiver_id.clone());
-        }
-        user_friend_store.borrow_mut().insert(sender_id.clone(), sender_friend_list);
+        Ok(result) => {
+            let (id_valid,): (bool,) = result;
+            if !id_valid {
+                return "Invalid User ID!".to_string();
+            }
+            let receiver_validation = ic_cdk::call::<(String,), (bool,)>(Principal::from_text("be2us-64aaa-aaaaa-qaabq-cai").unwrap(), "checkId", (receiver_id.clone(),)).await;
+            match receiver_validation {
+                Err(_err) => {
+                    return "Can't access store".to_string();
+                }
+                Ok(result) => {
+                    let (id_valid,): (bool,) = result;
+                    if !id_valid {
+                        return "Invalid User ID!".to_string();
+                    }
+                    DIRECT_MESSAGE_STORE.with(|direct_message_store| {
+                        let message = DirectMessage { 
+                            id: direct_message_store.borrow().clone().len(), 
+                            sender_id: sender_id.clone(), 
+                            receiver_id: receiver_id.clone(), 
+                            reply_id,
+                            content, 
+                            timestamp: "SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis()".to_string(), 
+                            // timestamp: DateTime::now().to_string(),            
+                            viewed: false 
+                        };
+                        direct_message_store.borrow_mut().push(message);
+                    });
+                    USER_FRIEND_STORE.with(|user_friend_store| {
+                        let mut sender_friend_list;
+                        let mut receiver_friend_list;
+                        if user_friend_store.borrow().get(&sender_id).is_none() {
+                            sender_friend_list = vec![];
+                        } else {
+                            sender_friend_list = user_friend_store.borrow().get(&sender_id).unwrap().clone();
+                        }
+                        if !sender_friend_list.iter().find(|&id| *id == receiver_id).is_some(){
+                            sender_friend_list.push(receiver_id.clone());
+                        }
+                        user_friend_store.borrow_mut().insert(sender_id.clone(), sender_friend_list);
 
-        if user_friend_store.borrow().get(&receiver_id).is_none() {
-            receiver_friend_list = vec![];
-        } else {
-            receiver_friend_list = user_friend_store.borrow().get(&receiver_id).unwrap().clone();
+                        if user_friend_store.borrow().get(&receiver_id).is_none() {
+                            receiver_friend_list = vec![];
+                        } else {
+                            receiver_friend_list = user_friend_store.borrow().get(&receiver_id).unwrap().clone();
+                        }
+                        if !receiver_friend_list.iter().find(|&id| *id == sender_id).is_some(){
+                            receiver_friend_list.push(sender_id);
+                        }
+                        user_friend_store.borrow_mut().insert(receiver_id, receiver_friend_list);
+                    });
+                    "Success!".to_string()
+                }
+            }
         }
-        if !receiver_friend_list.iter().find(|&id| *id == sender_id).is_some(){
-            receiver_friend_list.push(sender_id);
-        }
-        user_friend_store.borrow_mut().insert(receiver_id, receiver_friend_list);
-    });
-    "Success!".to_string()
+    }
 }
 
 pub fn get_friend_list(id: String) -> Vec<String> {
@@ -288,31 +326,52 @@ pub fn get_friend_list(id: String) -> Vec<String> {
     })
 }
 
-pub fn get_friend_messages(sender_id:String, receiver_id: String) -> Vec<DirectMessage> {
-    // if !id_utils::has_id(&receiver_id) || !id_utils::has_id(&sender_id) {
-    //     return vec![];
-    // }
-    // user friend store...
-    DIRECT_MESSAGE_STORE.with(|direct_message_store| {
-        let dms = direct_message_store.borrow().clone();
-        dms.iter().filter(|&message| 
-            message.sender_id == sender_id && 
-            message.receiver_id == receiver_id || 
-            message.sender_id == receiver_id && 
-            message.receiver_id == sender_id)
-        .map(|msg|{
-            let tmp = msg.clone();
-            DirectMessage { 
-                id: tmp.id, 
-                sender_id: tmp.sender_id, 
-                receiver_id: tmp.receiver_id, 
-                reply_id: tmp.reply_id, 
-                content: tmp.content, 
-                timestamp: tmp.timestamp, 
-                viewed: tmp.viewed
+pub async fn get_friend_messages(sender_id:String, receiver_id: String) -> Vec<DirectMessage> {
+    let sender_validation = ic_cdk::call::<(String,), (bool,)>(Principal::from_text("be2us-64aaa-aaaaa-qaabq-cai").unwrap(), "checkId", (sender_id.clone(),)).await;
+    match sender_validation {
+        Err(_err) => {
+            return vec![];
+        }
+        Ok(result) => {
+            let (id_valid,): (bool,) = result;
+            if !id_valid {
+                return vec![];
             }
-        }).collect()
-    })
+            let receiver_validation = ic_cdk::call::<(String,), (bool,)>(Principal::from_text("be2us-64aaa-aaaaa-qaabq-cai").unwrap(), "checkId", (receiver_id.clone(),)).await;
+            match receiver_validation {
+                Err(_err) => {
+                    return vec![];
+                }
+                Ok(result) => {
+                    let (id_valid,): (bool,) = result;
+                    if !id_valid {
+                        return vec![];
+                    }
+                    // user friend store...
+                    DIRECT_MESSAGE_STORE.with(|direct_message_store| {
+                        let dms = direct_message_store.borrow().clone();
+                        dms.iter().filter(|&message| 
+                            message.sender_id == sender_id && 
+                            message.receiver_id == receiver_id || 
+                            message.sender_id == receiver_id && 
+                            message.receiver_id == sender_id)
+                        .map(|msg|{
+                            let tmp = msg.clone();
+                            DirectMessage { 
+                                id: tmp.id, 
+                                sender_id: tmp.sender_id, 
+                                receiver_id: tmp.receiver_id, 
+                                reply_id: tmp.reply_id, 
+                                content: tmp.content, 
+                                timestamp: tmp.timestamp, 
+                                viewed: tmp.viewed
+                            }
+                        }).collect()
+                    })
+                }
+            }
+        }
+    }
 }
 
 pub fn view_message() -> bool {true}
